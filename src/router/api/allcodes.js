7227c3354b192275b45425646aca2c169b5f6972
LAPTOP-ULH3QS5V\Admin <hoangmanuciansss@gmail.cm>
2022-05-27 22:52:27 +0700

    config backend

--- src/router/api/allcodes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import Allcode from '../../controller/allcodeController'
-const router = express.Router();
-let initRouteApiAllcode = (app) => {
-    router.get('/allcode', Allcode.handleGetAllcode)
-    app.use('/api/v1', router);
-}
-
-module.exports = initRouteApiAllcode;
--- src/server.js
+++ src/server.js
@@ -1,25 +1,14 @@
 import express from "express";
 import viewEngine from "../src/config/viewEngine";
 import bodyParser from "body-parser";
-import initRouteWeb from "../src/router/web";
 import initRouteApi from "../src/router/api/userApi";
-import initRouteApiAllcode from "./router/api/allcodes";
-import initRouteDoctorApi from "./router/api/doctor";
-import initRouteApiSpeciality from "./router/api/speciality";
-import initRouteApiClinic from "./router/api/clinic";
 import connectDB from "../src/config/connectDB";
 import dotenv from "dotenv";
 // import cors from 'cors'
 dotenv.config();
 const app = express();
 const port = 8080;
-// //using cors
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     credentials: true,            //access-control-allow-credentials:true
-//     optionSuccessStatus: 200
-// }
-// app.use(cors(corsOptions));
+
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -59,13 +48,9 @@ connectDB();
 // config viewEngine
 viewEngine(app);
 // init routerWeb
-initRouteWeb(app);
-// initRouteApi
+
 initRouteApi(app);
-initRouteApiAllcode(app);
-initRouteDoctorApi(app);
-initRouteApiSpeciality(app);
-initRouteApiClinic(app);
+
 // start server
 app.listen(port, () => {
     console.log(`Server on:http://localhost:${port}`);
